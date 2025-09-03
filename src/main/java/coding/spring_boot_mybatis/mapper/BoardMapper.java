61108dd4a882a0d69bc664bbc23c85f26632d192
MA-Ha-eun <1thecalla@gmail.com>
2025-09-03 18:37:13 +0900

    mybatis 실습 완료

--- src/main/java/coding/spring_boot_mybatis/mapper/BoardMapper.java
+++ src/main/java/coding/spring_boot_mybatis/mapper/BoardMapper.java
@@ -7,19 +7,26 @@ import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-    @Select("SELECT * FROM board")
+//    @Select("SELECT * FROM board")
+//    List<Post> findAll();
+//
+//    @Select("SELECT * FROM board where writer = #{writer}")
+//    Post findByWriter(String writer);
+//
+//    @Insert("INSERT INTO board (title, content, writer) VALUES (#{title}, #{content}, #{writer})")
+//    @Options(useGeneratedKeys = true, keyProperty = "id")
+//    void insert(Post post);
+//
+//    @Update("Update board SET title = #{title}, content = #{content} WHERE id = #{id}")
+//    void update(Post post);
+//
+//    @Delete("DELETE FROM board WHERE id = #{id}")
+//    void delete(Long id);
+
+    // XML 기반 매퍼
     List<Post> findAll();
-
-    @Select("SELECT * FROM board where writer = #{writer}")
     Post findByWriter(String writer);
-
-    @Insert("INSERT INTO board (title, content, writer) value (#{title}, #{content}, #{writer})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(Post post);
-
-    @Update("Update board SET title = #{title}, content = #{content} WHERE id = #{id}")
     void update(Post post);
-
-    @Delete("DELETE FROM board WHERE id = #{id}")
     void delete(Long id);
 }
--- src/main/java/coding/spring_boot_mybatis/mapper/UserMapper.java
+++ src/main/java/coding/spring_boot_mybatis/mapper/UserMapper.java
@@ -8,31 +8,40 @@ import java.util.List;
 @Mapper // 해당 인터페이스가 mybatis mapper 임을 나타냄
 public interface UserMapper {
 
-    // case1. 어노테이션 기반 매퍼
-    // - 간단한 SQL(쿼리)의 경우 간결하게 표현
-    // - Java 코드 내에서 SQL문을 직접 볼 수 있어 즉각적인 이해 가능
-    // - @Select, @Insert, @Update, @Delete 어노테이션 사용
-    @Select("SELECT * FROM users")
-    List<User> findAll(); // UserService가 호출
-
-    @Select("SELECT * FROM users where id = #{id}")
-    User findById(Long id);
-
-    @Insert("INSERT INTO users (username, email) value (#{username}, #{email})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
-    // Insert 작업에 대한 추가 옵션 설정
-    // - useGeneratedKeys = true; 데이터베이스에서 자동 생성되는 키 (auto-increment pk) 를 사용하겠다
-    // - keyProperty = "id"; 생성된 키 값을 User 객체의 id 필드에 저장하겠다는 의미
-    // -> insert 메서드 호출 후 전달된 User 객체의 id 필드는 데이터베이스에서 생성된 실제 id 값으로 업데이트
-    // -> 새로 삽입된 행의 id 를 알 수 있게 되어 이후 작업에 해당 id 를 사용 가능
-    // 만약 users 테이블의 id(pk) 를 수동으로 설정한다면 Options 어노테이션 필요x
-    void insert(User user);
-
-    @Update("Update users SET username = #{username}, email = #{email} WHERE id = #{id}")
-    void update(User user);
-
-    @Delete("DELETE FROM users WHERE id = #{id}")
-    void delete(Long id);
+//    // case1. 어노테이션 기반 매퍼
+//    // - 간단한 SQL(쿼리)의 경우 간결하게 표현
+//    // - Java 코드 내에서 SQL문을 직접 볼 수 있어 즉각적인 이해 가능
+//    // - @Select, @Insert, @Update, @Delete 어노테이션 사용
+//    @Select("SELECT * FROM users")
+//    List<User> findAll(); // UserService가 호출
+//
+//    @Select("SELECT * FROM users where id = #{id}")
+//    User findById(Long id);
+//
+//    @Insert("INSERT INTO users (username, email) VALUES (#{username}, #{email})")
+//    @Options(useGeneratedKeys = true, keyProperty = "id")
+//    // Insert 작업에 대한 추가 옵션 설정
+//    // - useGeneratedKeys = true; 데이터베이스에서 자동 생성되는 키 (auto-increment pk) 를 사용하겠다
+//    // - keyProperty = "id"; 생성된 키 값을 User 객체의 id 필드에 저장하겠다는 의미
+//    // -> insert 메서드 호출 후 전달된 User 객체의 id 필드는 데이터베이스에서 생성된 실제 id 값으로 업데이트
+//    // -> 새로 삽입된 행의 id 를 알 수 있게 되어 이후 작업에 해당 id 를 사용 가능
+//    // 만약 users 테이블의 id(pk) 를 수동으로 설정한다면 Options 어노테이션 필요x
+//    void insert(User user);
+//
+//    @Update("Update users SET username = #{username}, email = #{email} WHERE id = #{id}")
+//    void update(User user);
+//
+//    @Delete("DELETE FROM users WHERE id = #{id}")
+//    void delete(Long id);
 
     // case2. XML 기반 매퍼
+    // - 복잡한 SQL 쿼리를 쉽게 관리
+    // - 동적 SQL 작성 편리
+    // - SQL 과 Java 코드를 분리해 관리
+    // - 대규모 프로젝트나 복잡한 데이터 조작 적합
+    List<User> findAll();
+    User findById(Long id);
+    void insert(User user);
+    void update(User user);
+    void delete(Long id);
 }
